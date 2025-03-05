78bbf88ff18a317d2851da4c3130d9aea0c3e103
Aritra Bose <aritrabose2003@gmail.com>
2025-03-05 15:56:54 +0530

    Change port number to 5050 to prevent Multithreaded Rendezvous error for clients

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Server Configuration
 NUM_ROUNDS = 50  # Number of federated learning rounds
-SERVER_ADDRESS = "localhost:8080"
+SERVER_ADDRESS = "localhost:5050"
 
 # Dataset Paths
 SERVER_TEST_PATH = "dataset/server/test/"
